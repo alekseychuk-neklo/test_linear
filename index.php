1ba7ab47e0dadd62bd83065ede2624a39779445d
igoraleksejcuk <igor.aleksejcuk@neklo.com>
2019-10-14 10:27:13 +0300

    two libs

--- index.php
+++ index.php
@@ -2,40 +2,27 @@
 
 require_once './vendor/autoload.php';
 require_once './app/src/func.php';
-use app\app\ExLinear;
-use mnshankar\LinearRegression\Regression;
 
-$testData = [[245, 1400],[312, 1600], [279, 1700], [308, 1850], [199, 1100]];
-$linear = new ExLinear();
-$linear->setSourceSequence($testData);
-$linear->calculate();
-echo "first way ";
-var_dump($linear->getEquation());
-$chart =[];
-$res =$linear->getReultSequense();
-$i = 0;
-foreach ($testData as $value){
-    $chart['labels'][] = $value[0];
-    $chart['dataset1'][] = $value[1];
-    $chart['dataset2'][] = $res[$i][1];
-    $i++;
-}
+use Phpml\Dataset\CsvDataset;
+use Phpml\Regression\LeastSquares;
+
+$regression = new LeastSquares();
+
+$dataset = new CsvDataset('train.csv', 1, true);
 
 
-$x_arr = [];
-$y_arr = [];
-foreach ($testData as $elements){
-    $x_arr[] = $elements[0];
-    $y_arr[] = $elements[1];
-}
-echo "second way";
-var_dump( linear_regression($x_arr, $y_arr) );
+$regression->train($dataset->getSamples(), $dataset->getTargets());
+print_r($regression->getCoefficients());
+echo "<br>";
+print_r('intercept: '.$regression->getIntercept());
 
-//$reg = new Regression();
-//$reg->setX($testData);
-//$reg->setY($testData);
-//$reg->compute();
-//print_r($reg);
-include 'app/src/chart.php';
-
+$dataset  = new CsvDataset('train_2.csv', 2, true);
+echo "<pre>";
+//print_r($dataset->getTargets());
+echo "</pre>";
+$regression = new LeastSquares();
+$regression->train($dataset->getSamples(), $dataset->getTargets());
+print_r($regression->getCoefficients());
+echo "<br>";
+print_r('intercept: '.$regression->getIntercept());
 
